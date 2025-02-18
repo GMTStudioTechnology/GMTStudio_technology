cfa2d37a21a1de5517e804a156c64250bdfb1da9
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2025-02-18 08:16:35 +0800

    ((꜆꜄꜆ ˙꒳˙)꜆꜄꜆ｵﾗｵﾗｵﾗｵﾗ((꜆꜄꜆ ˙꒳˙)꜆꜄꜆ｵﾗｵﾗｵﾗｵﾗ

--- src/components/layout/onlineTools.tsx
+++ src/components/layout/onlineTools.tsx
@@ -6,7 +6,7 @@ const OnlineTools: React.FC = () => {
     <div className="bg-black text-white min-h-screen">
       <Navbar />
       <div className="container mx-auto flex flex-col items-center justify-center min-h-[calc(100vh-80px)] pt-20">
-        <h1 className="text-2xl mb-4 sm:text-4xl md:text-6xl lg:text-8xl">GMTStudio Online tools </h1>
+        <h1 className="text-2xl mb-4 sm:text-4xl md:text-6xl lg:text-8xl">((꜆꜄꜆ ˙꒳˙)꜆꜄꜆ｵﾗｵﾗｵﾗｵﾗ</h1>
         <p className="text-lg mb-8 sm:text-lg md:text-xl lg:text-2xl">We let this space left with emptiness at here due to the fact that we haven't develop any of it </p>
       </div>
     </div>
